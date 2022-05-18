2513532b9e1e8206bacdf0ab3414b42900de7ac0
Thomas Schmidt <tbs@conhea.dk>
2022-05-19 00:47:35 +0200

    Introduced medcomtools.maskIdentifiers function

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
+import { utils } from 'mocha';
 import * as vscode from 'vscode';
 
 // this method is called when your extension is activated
@@ -10,13 +11,18 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "medcomtools" is now active!');
 
+	const xmlString = vscode.window.activeTextEditor?.document.getText();
+
+	
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('medcomtools.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('medcomtools.maskIdentifiers', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from MedComTools!');
+
+
 	});
 
 	context.subscriptions.push(disposable);
